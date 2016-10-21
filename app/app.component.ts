21af6872ef27b2bd744062e4b70fdd602df39373
evan <stewarea>
2016-10-21 11:47:51 -0700

    add styling and fonts

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,38 +4,40 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'my-app',
   template: `
-  <div class ="container">
-    <div class="jumbotron">
-      <h1>Meal Tracker Hacker</h1>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-md-8">
-          <div class="row well">
-            <div class="col-md-6 add">
-              <meal-new (newMealSender)="addMeal($event)"
-              ></meal-new>
+  <div class="body">
+    <div class ="container">
+      <div class="jumbotron">
+        <p>Meal Tracker Hacker</p>
+      </div>
+      <hr>
+      <div class="row">
+          <div class="col-md-8">
+            <div class="row well">
+              <div class="col-md-6 add">
+                <meal-new (newMealSender)="addMeal($event)"
+                ></meal-new>
+              </div>
+
+              <div class="col-md-6 edit">
+                <meal-edit [childSelectedMeal]="selectedMeal" (doneEditingClickedSender)="finishedEditing()"
+                ></meal-edit>
+              </div>
+            </div>
+            <div class="row well">
+              <div class="col-md-12 list">
+                <meal-list [childMealList]="masterMealList" (editButtonClickSender)="showEdit($event)"
+                ></meal-list>
+              </div>
             </div>
 
-            <div class="col-md-6 edit">
-              <meal-edit [childSelectedMeal]="selectedMeal" (doneEditingClickedSender)="finishedEditing()"
-              ></meal-edit>
-            </div>
+
           </div>
-          <div class="row well">
-            <div class="col-md-12 list">
-              <meal-list [childMealList]="masterMealList" (editButtonClickSender)="showEdit($event)"
-              ></meal-list>
-            </div>
+          <div class="col-sm-1">
           </div>
-
-
-        </div>
-        <div class="col-sm-1">
-        </div>
-        <div class="col-md-3 well">
-            <meal-display [childMealList]="masterMealList"
-            ></meal-display>
+          <div class="col-md-3 well">
+              <meal-display [childMealList]="masterMealList"
+              ></meal-display>
+          </div>
         </div>
       </div>
     </div>
