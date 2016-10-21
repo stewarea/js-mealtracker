b26f2ba24da8046a73b84abfd6a859ed986c36c9
evan <stewarea>
2016-10-21 08:25:25 -0700

    set up basic layout, add object model

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MealListComponent } from './meal-list.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+  MealListComponent,
+FormsModule ],
   bootstrap:    [ AppComponent ]
 })
 
